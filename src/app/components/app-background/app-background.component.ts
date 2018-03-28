86e599bc5a09d81ae2b391d9bb7d3b89a767dd00
Lucas Seinfeld <l.seinfeld@kiwi-suite.com>
2018-03-28 15:14:56 +0200

    configurable background component

--- src/app/components/app-background/app-background.component.ts
+++ src/app/components/app-background/app-background.component.ts
@@ -1,16 +1,25 @@
-import {Component} from '@angular/core';
-import 'rxjs/add/operator/filter';
+import {Component, Input} from '@angular/core';
 import {ConfigurationService} from '../../../kiwi/services';
 
 @Component({
     selector: 'app-background',
     template: `
-        <div style="position: fixed; top:0; left:0; right: 0; bottom: 0; z-index:-1; opacity: 0.7; background-position: center center; background-size: cover; background-image: url(assets/img/kiwi-background.jpg) ">
+        <div [ngStyle]="{opacity: opacity, 'background-color': color, 'background-position': position, 'background-size': size, 'background-image': url}"
+             style="position: fixed; top:0; left:0; right: 0; bottom: 0; z-index:-1;">
         </div>`
 })
 export class AppBackgroundComponent {
-    backgroundUrl: string;
+    @Input('color') color = 'transparent';
+    @Input('position') position = 'center center';
+    @Input('opacity') opacity = 0.5;
+    @Input('repeat') repeat = 'repeat';
+    @Input('size') size = 'cover';
+    @Input('url') _url = null;
 
     constructor(private config: ConfigurationService) {
+    }
+
+    get url() {
+        return 'url(' + this._url + ')';
     }
 }
