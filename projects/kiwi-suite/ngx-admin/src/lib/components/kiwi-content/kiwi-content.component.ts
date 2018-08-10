eefbf1717aa531a7d1187c1c7e9784737ab144bb
Michael Wohlfahrter <m.wohlfahrter@gmail.com>
2018-08-10 13:32:36 +0200

    header buttons

--- projects/kiwi-suite/ngx-admin/src/lib/components/kiwi-content/kiwi-content.component.ts
+++ projects/kiwi-suite/ngx-admin/src/lib/components/kiwi-content/kiwi-content.component.ts
@@ -1,34 +1,24 @@
-import { AfterViewInit, Component, ContentChild, ElementRef, HostListener, Input, TemplateRef, ViewChild, } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild, } from '@angular/core';
 
 @Component({
   selector: 'kiwi-content',
   templateUrl: './kiwi-content.component.html',
 })
 export class KiwiContentComponent implements AfterViewInit {
-  @ContentChild('content')
-  mainContent: TemplateRef<any>;
-
-  @ContentChild('aside')
-  asideContent: TemplateRef<any>;
-
-  @ContentChild('header')
-  headerContent: TemplateRef<any>;
-
   @ViewChild('header') header: ElementRef;
 
-  @Input() title = '';
-
   headerHeight = 0;
+  paddingTop = 26;
 
   @HostListener('window:resize') onResize() {
     if (this.header) {
-      this.headerHeight = this.header.nativeElement.clientHeight;
+      this.headerHeight = this.header.nativeElement.clientHeight + this.paddingTop;
     }
   }
 
   ngAfterViewInit() {
     setTimeout(() => {
-      this.headerHeight = this.header.nativeElement.clientHeight;
+      this.headerHeight = this.header.nativeElement.clientHeight + this.paddingTop;
     });
   }
 }
