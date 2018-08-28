94e80ffbfe630d4c9ec230942d5303f852db69be
Michael Wohlfahrter <m.wohlfahrter@gmail.com>
2018-08-28 13:21:59 +0200

    style fix

--- projects/kiwi-suite/ngx-admin/src/lib/forms/wrappers/form-field-only.wrapper.ts
+++ projects/kiwi-suite/ngx-admin/src/lib/forms/wrappers/form-field-only.wrapper.ts
@@ -7,12 +7,6 @@ import { FieldWrapper } from '@ngx-formly/core';
     <div [class.has-error]="showError">
       <div class="kiwi-form-control-container">
         <ng-template #fieldComponent></ng-template>
-
-        <div *ngIf="showError" class="invalid-feedback" [style.display]="'block'">
-          <formly-validation-message [field]="field"></formly-validation-message>
-        </div>
-
-        <small *ngIf="to.description" class="form-text text-muted">{{ to.description }}</small>
       </div>
     </div>
   `,
