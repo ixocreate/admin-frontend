828e7d76c59021d7b131d8c960b7e19f102dd667
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-11-15 13:56:32 +0100

    removed permission check

--- projects/kiwi-suite/ngx-admin/src/lib/kiwi-admin.routing.ts
+++ projects/kiwi-suite/ngx-admin/src/lib/kiwi-admin.routing.ts
@@ -41,7 +41,7 @@ export const routes: Routes = [
   {
     path: '',
     component: DefaultLayoutComponent,
-    canActivate: [AuthGuard, PermissionGuard],
+    canActivate: [AuthGuard],
     children: [
       {path: '', component: RedirectComponent},
       {
