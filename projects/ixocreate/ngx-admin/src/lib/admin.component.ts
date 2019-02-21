d90f5bc801f591b42ff97d29e2be8ac5810dcbe2
Michael Wohlfahrter <m.wohlfahrter@ixolit.com>
2019-02-21 14:36:25 +0100

    fix not available locale

--- projects/ixocreate/ngx-admin/src/lib/admin.component.ts
+++ projects/ixocreate/ngx-admin/src/lib/admin.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { PageTitleService } from './services/page-title.service';
 import Quill from 'quill';
+import { ConfigService } from './services/config.service';
+import { LocalStorageService } from './services/local-storage.service';
 
 const Parchment = Quill.import('parchment');
 const LineBreakClass = new Parchment.Attributor.Class('linebreak', 'linebreak', {
@@ -15,8 +17,19 @@ Quill.register('formats/linebreak', LineBreakClass);
 })
 export class AdminComponent {
 
-  constructor(pageTitle: PageTitleService) {
+  constructor(pageTitle: PageTitleService, config: ConfigService, localStorage: LocalStorageService) {
     pageTitle.init();
+
+    const selectedLocale = localStorage.getItem(LocalStorageService.SELECTED_LANGUAGE, config.config.intl.default);
+    let localeFound = false;
+    for (const locale of config.config.intl.locales) {
+      if (locale.locale === selectedLocale) {
+        localeFound = true;
+      }
+    }
+    if (!localeFound) {
+      localStorage.setItem(LocalStorageService.SELECTED_LANGUAGE, config.config.intl.locales[0].locale);
+    }
   }
 
 }
