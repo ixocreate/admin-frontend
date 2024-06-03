1d56b72b7c5bddfe222227079b45abe3f49ac903
Michael Wohlfahrter <m.wohlfahrter@ixopay.com>
2024-06-03 13:05:27 +0200

    allow superscript in editor

--- projects/ixocreate/ngx-admin/src/lib/forms/quill-modules.config.ts
+++ projects/ixocreate/ngx-admin/src/lib/forms/quill-modules.config.ts
@@ -3,6 +3,7 @@ export const quillModules = {
     container: [
       ['undo', 'redo'],
       ['bold', 'italic', 'underline', 'strike'],
+      [{ script: 'sub'}, { script: 'super' }],
       [{list: 'ordered'}, {list: 'bullet'}],
       [{header: [1, 2, 3, 4, 5, 6, false]}],
       ['clean'],
